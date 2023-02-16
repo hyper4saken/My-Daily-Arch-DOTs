0074ded1d08ccaf77b99129cc50b90a33bc5481c
Hyper4Saken <premtshering91@gmail.com>
2023-02-16 22:22:35 +0530

    dooooooooom emacs config

---
+++ .doom.d/config.el
@@ -0,0 +1,65 @@
+(setq user-full-name "Hyper4Saken"
+      user-mail-address "hyper4saken@protonmail.com")
+
+;;fonts
+(setq doom-font (font-spec :family "Hack Nerd Font Mono" :size 18))
+
+;; theme
+(setq doom-theme 'doom-henna)
+
+;; relative number
+(setq display-line-numbers-type t)
+
+(map! :leader
+      :desc "Org babel tangle" "m B" #'org-babel-tangle)
+(after! org
+  (setq org-directory "/mnt/Backup/Org/"
+        org-default-notes-file (expand-file-name "notes.org" org-directory)
+        org-ellipsis " ▼ "
+        org-superstar-headline-bullets-list '("◉" "●" "○" "◆" "●" "○" "◆")
+        org-superstar-itembullet-alist '((?+ . ?➤) (?- . ?✦)) ; changes +/- symbols in item lists
+        org-log-done 'time
+        org-hide-emphasis-markers t
+        ;; ex. of org-link-abbrev-alist in action
+        ;; [[arch-wiki:Name_of_Page][Description]]
+        org-link-abbrev-alist    ; This overwrites the default Doom org-link-abbrev-list
+          '(("google" . "http://www.google.com/search?q=")
+            ("arch-wiki" . "https://wiki.archlinux.org/index.php/")
+            ("ddg" . "https://duckduckgo.com/?q=")
+            ("wiki" . "https://en.wikipedia.org/wiki/"))
+        org-table-convert-region-max-lines 20000
+        org-todo-keywords        ; This overwrites the default Doom org-todo-keywords
+          '((sequence
+             "TODO(t)"           ; A task that is ready to be tackled
+             "BLOG(b)"           ; Blog writing assignments
+             "GYM(g)"            ; Things to accomplish at the gym
+             "PROJ(p)"           ; A project that contains other tasks
+             "VIDEO(v)"          ; Video assignments
+             "WAIT(w)"           ; Something is holding up this task
+             "|"                 ; The pipe necessary to separate "active" states and "inactive" states
+             "DONE(d)"           ; Task has been completed
+             "CANCELLED(c)" )))) ; Task has been cancelled
+
+(after! org
+  (setq org-roam-directory "/mnt/Backup/Org/roam/"
+        org-roam-graph-viewer "/usr/bin/brave"))
+
+(map! :leader
+      (:prefix ("n r" . "org-roam")
+       :desc "Completion at point" "c" #'completion-at-point
+       :desc "Find node"           "f" #'org-roam-node-find
+       :desc "Show graph"          "g" #'org-roam-graph
+       :desc "Insert node"         "i" #'org-roam-node-insert
+       :desc "Capture to node"     "n" #'org-roam-capture
+       :desc "Toggle roam buffer"  "r" #'org-roam-buffer-toggle))
+
+(xterm-mouse-mode 1)
+
+(after! neotree
+  (setq neo-smart-open t
+        neo-window-fixed-size nil))
+(after! doom-themes
+  (setq doom-neotree-enable-variable-pitch t))
+(map! :leader
+      :desc "Toggle neotree file viewer" "t n" #'neotree-toggle
+      :desc "Open directory in neotree"  "d n" #'neotree-dir)
---
+++ .doom.d/packages.el
@@ -0,0 +1,38 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
+
+
+;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
+;(package! some-package)
+
+(package! flycheck-aspell)
+(package! calfw)
+(package! calfw-org)
+;;(package! dashboard)
+(package! dired-open)
+(package! dired-subtree)
+(package! dirvish)
+(package! dmenu)
+(package! ednc)
+(package! emojify)
+(package! evil-tutor)
+(package! exwm)
+(package! ivy-posframe)
+(package! mw-thesaurus)
+(package! org-auto-tangle)
+(package! org-web-tools)
+(package! ox-gemini)
+(package! peep-dired)
+(package! password-store)
+(package! rainbow-mode)
+(package! resize-window)
+(package! tldr)
+(package! wc-mode)
+(package! beacon)
+(package! clippy)
+(package! minimap)
+(package! olivetti)
