337fb2a059b2b1954b33c9e7c7aac257298e5091
Hyper4Saken <46641940+hyper4saken@users.noreply.github.com>
2023-02-20 21:46:23 +0530

    terminal  shortcut added

--- doom/config.el
+++ doom/config.el
@@ -1,5 +1,5 @@
 (setq user-full-name "Hyper4Saken"
-      user-mail-address " ")
+      user-mail-address "hyper4saken@protonmail.com")
 
 ;;fonts
 (setq doom-font (font-spec :family "JetBrains Mono" :size 20)
@@ -156,3 +156,20 @@
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)))
+
+;;terminal
+(setq shell-file-name "/bin/zsh"
+      vterm-max-scrollback 5000)
+(setq eshell-rc-script "~/.config/doom/eshell/profile"
+      eshell-aliases-file "~/.config/doom/eshell/aliases"
+      eshell-history-size 5000
+      eshell-buffer-maximum-lines 5000
+      eshell-hist-ignoredups t
+      eshell-scroll-to-bottom-on-input t
+      eshell-destroy-buffer-when-process-dies t
+      eshell-visual-commands'("bash" "fish" "htop" "ssh" "top" "zsh"))
+(map! :leader
+      :desc "Eshell"                 "e s" #'eshell
+      :desc "Eshell popup toggle"    "e t" #'+eshell/toggle
+      :desc "Counsel eshell history" "e h" #'counsel-esh-history
+      :desc "Vterm popup toggle"     "v t" #'+vterm/toggle)
