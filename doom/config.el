dd1494054d9eb3d029c378f17f07e6ce41a48432
Hyper4Saken <premtshering91@gmail.com>
2023-02-24 11:43:40 +0530

    org customize

--- doom/config.el
+++ doom/config.el
@@ -1,7 +1,9 @@
 (setq user-full-name "Hyper4Saken"
       user-mail-address "hyper4saken@protonmail.com")
 
-;;fonts
+;; -------------------------------------------------------------------------------
+;;                                 FONTS & THEME SETTINGS
+;; ------------------------------------------------------------------------------
 (setq doom-font (font-spec :family "JetBrains Mono" :size 20)
       doom-variable-pitch-font (font-spec :family "Ubuntu" :size 20)
       doom-big-font (font-spec :family "JetBrains Mono" :size 24))
@@ -13,67 +15,23 @@
   '(font-lock-keyword-face :slant italic))
 
 ;; theme
-;;(setq doom-theme 'doom-henna)
-;;(setq doom-theme 'doom-tomorrow-night)
-;;(setq doom-theme 'doom-tokyo-night)
 (setq doom-theme 'doom-ayu-dark)
 
 ; relative number
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode 1)
 
-(map! :leader
-      :desc "Org babel tangle" "m B" #'org-babel-tangle)
-(after! org
-  (setq org-directory "/mnt/Backup/Org/"
-        org-default-notes-file (expand-file-name "notes.org" org-directory)
-        org-ellipsis " ▼ "
-        org-superstar-headline-bullets-list '("❂" "✪" "❆" "◉" "○" "●" "◆")
-        org-superstar-itembullet-alist '((?+ . ?✠) (?- . ?✦)) ; changes +/- symbols in item lists
-        org-log-done 'time
-        org-hide-emphasis-markers t
-        ;; ex. of org-link-abbrev-alist in action
-        ;; [[arch-wiki:Name_of_Page][Description]]
-        org-link-abbrev-alist    ; This overwrites the default Doom org-link-abbrev-list
-          '(("google" . "http://www.google.com/search?q=")
-            ("arch-wiki" . "https://wiki.archlinux.org/index.php/")
-            ("ddg" . "https://duckduckgo.com/?q=")
-            ("wiki" . "https://en.wikipedia.org/wiki/"))
-        org-table-convert-region-max-lines 20000
-        org-todo-keywords        ; This overwrites the default Doom org-todo-keywords
-          '((sequence
-             "TODO(t)"           ; A task that is ready to be tackled
-             "BLOG(b)"           ; Blog writing assignments
-             "GYM(g)"            ; Things to accomplish at the gym
-             "PROJ(p)"           ; A project that contains other tasks
-             "VIDEO(v)"          ; Video assignments
-             "WAIT(w)"           ; Something is holding up this task
-             "|"                 ; The pipe necessary to separate "active" states and "inactive" states
-             "DONE(d)"           ; Task has been completed
-             "CANCELLED(c)" )))) ; Task has been cancelled
-
-(after! org
-  (setq org-roam-directory "/mnt/Backup/Org/roam/"
-        org-roam-graph-viewer "/usr/bin/brave"))
-
-(map! :leader
-      (:prefix ("n r" . "org-roam")
-       :desc "Completion at point" "c" #'completion-at-point
-       :desc "Find node"           "f" #'org-roam-node-find
-       :desc "Show graph"          "g" #'org-roam-graph
-       :desc "Insert node"         "i" #'org-roam-node-insert
-       :desc "Capture to node"     "n" #'org-roam-capture
-       :desc "Toggle roam buffer"  "r" #'org-roam-buffer-toggle))
-
-;; org-headers font size
-(custom-theme-set-faces
-     'user
-     `(org-level-4 ((t ( :height 1.2))))
-     `(org-level-3 ((t ( :height 1.5))))
-     `(org-level-2 ((t ( :height 1.8))))
-     `(org-level-1 ((t ( :height 2.00))))
-     )
-
+;; modline fonts
+(set-face-attribute 'mode-line nil :font "Ubuntu Mono-15")
+(setq doom-modeline-height 30     ;; sets modeline height
+      doom-modeline-bar-width 5   ;; sets right bar width
+      doom-modeline-persp-name t  ;; adds perspective name to modeline
+      doom-modeline-persp-icon t   ;; adds folder icon next to persp name
+      doom-modeline-major-mode-icon t  ;; if icons failed major icons selected
+      doom-modeline--buffer-file-state-icon t)  ;;show icon in bufferline
+;; -------------------------------------------------------------------------------
+;;                                  EXTRA SETTINGS
+;; ------------------------------------------------------------------------------
 (xterm-mouse-mode 1)
 
 (after! neotree
@@ -83,18 +41,13 @@
 ;;(after! doom-themes-neotree-config
 (setq doom-themes-neotree-file-icons t
    doom-neotree-enable-variable-pitch t)
-;;)
+;; nerd-tree force use icons
+(setq inhibit-compacting-font-caches t)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 (map! :leader
       :desc "Toggle neotree file viewer" "t n" #'neotree-toggle
       :desc "Open directory in neotree"  "d n" #'neotree-dir)
 
-(set-face-attribute 'mode-line nil :font "Ubuntu Mono-15")
-(setq doom-modeline-height 30     ;; sets modeline height
-      doom-modeline-bar-width 5   ;; sets right bar width
-      doom-modeline-persp-name t  ;; adds perspective name to modeline
-      doom-modeline-persp-icon t   ;; adds folder icon next to persp name
-      doom-modeline-major-mode-icon t  ;; if icons failed major icons selected
-      doom-modeline--buffer-file-state-icon t)  ;;show icon in bufferline
 
 ;; tabs with icons
 (setq centaur-tabs-set-bar 'over
@@ -125,6 +78,55 @@
        :desc "Toggle line highlight globally" "H" #'global-hl-line-mode
        :desc "Toggle truncate lines"          "t" #'toggle-truncate-lines))
 
+;; -------------------------------------------------------------------------------
+;;                                  ORG-MODE SETTINGS
+;; ------------------------------------------------------------------------------
+
+(map! :leader
+      :desc "Org babel tangle" "m B" #'org-babel-tangle)
+(after! org
+  (setq org-directory "/mnt/Backup/Org/"
+        org-default-notes-file (expand-file-name "notes.org" org-directory)
+        org-ellipsis " ▼ "
+        org-superstar-headline-bullets-list '("❂" "✪" "❆" "◉" "○" "●" "◆")
+        org-superstar-itembullet-alist '((?+ . ?✠) (?- . ?✦)) ; changes +/- symbols in item lists
+        org-log-done 'time
+        org-hide-emphasis-markers t
+        ;; ex. of org-link-abbrev-alist in action
+        ;; [[arch-wiki:Name_of_Page][Description]]
+        org-link-abbrev-alist    ; This overwrites the default Doom org-link-abbrev-list
+          '(("google" . "http://www.google.com/search?q=")
+            ("arch-wiki" . "https://wiki.archlinux.org/index.php/")
+            ("ddg" . "https://duckduckgo.com/?q=")
+            ("wiki" . "https://en.wikipedia.org/wiki/"))
+        org-table-convert-region-max-lines 20000
+        org-todo-keywords        ; This overwrites the default Doom org-todo-keywords
+          '((sequence
+             "TODO(t)"           ; A task that is ready to be tackled "BLOG(b)"           ; Blog writing assignments "GYM(g)"            ; Things to accomplish at the gym
+             "PROJ(p)"           ; A project that contains other tasks
+             "VIDEO(v)"          ; Video assignments
+             "WAIT(w)"           ; Something is holding up this task
+             "|"                 ; The pipe necessary to separate "active" states and "inactive" states
+             "DONE(d)"           ; Task has been completed
+             "CANCELLED(c)" )))) ; Task has been cancelled
+
+(after! org
+  (setq org-roam-directory "/mnt/DATA/Roam/" org-roam-graph-viewer "/usr/bin/brave")
+        (set-face-attribute 'org-level-1 nil :font "JetBrains Mono" :weight 'bold :height 1.8)
+        (set-face-attribute 'org-level-2 nil :font "JetBrains Mono" :weight 'bold :height 1.6)
+        (set-face-attribute 'org-level-3 nil :font "JetBrains Mono" :weight 'bold :height 1.3)
+        (set-face-attribute 'org-level-4 nil :font "JetBrains Mono" :weight 'bold :height 1.1)
+        (set-face-attribute 'org-level-5 nil :font "JetBrains Mono" :weight 'bold :height 1.0))
+
+(map! :leader
+      (:prefix ("n r" . "org-roam")
+       :desc "Completion at point" "c" #'completion-at-point
+       :desc "Find node"           "f" #'org-roam-node-find
+       :desc "Show graph"          "g" #'org-roam-graph
+       :desc "Insert node"         "i" #'org-roam-node-insert
+       :desc "Capture to node"     "n" #'org-roam-capture
+       :desc "Toggle roam buffer"  "r" #'org-roam-buffer-toggle))
+
 ;; calendar config and dates
 (defun hyper/insert-todays-date (prefix)
   (interactive "P")
@@ -145,7 +147,36 @@
         :desc "Insert any date"    "a" #'hyper/insert-any-date
         :desc "Insert todays date" "t" #'hyper/insert-todays-date))
 
-;; markdown
+;; org-babel
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((python . t)))
+
+;;org-download
+;;This extension facilitates moving images from browser filesystems
+(require 'org-download)
+;; Drag-and-drop to `dired`
+(add-hook 'dired-mode-hook 'org-download-enable)
+;; Screen captured is saved on particular folder
+
+;;(after! org-download
+;;  (setq-default org-download-image-dir "/mnt/Backup/Org/notes_images"))
+(setq org-download-method 'custom)
+(setq org-download-custom-functions
+      '(("h" . (lambda () (org-download--dir (org-get-title t t t t))))))
+
+;; custom keybinding for org-download
+(map! :leader
+      (:prefix ("i v" . "Org Download Mode")
+        :desc "Paste Image"    "a" #'org-download-clipboard
+        :desc "Screen Capture" "s" #'org-download-screenshot))
+
+;; open org file header folded on startup open
+(setq org-startup-folded t)
+
+;; -------------------------------------------------------------------------------
+;;                                  MARKDOWN SETTINGS
+;; ------------------------------------------------------------------------------
 (custom-set-faces
  '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
  '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.7))))
@@ -155,12 +186,9 @@
  '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.3))))
  '(markdown-header-face-6 ((t (:inherit markdown-header-face :height 1.2)))))
 
-;; org-babel
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)))
-
-;;terminal
+;; -------------------------------------------------------------------------------
+;;                                  TERMINAL SETTINGS
+;; ------------------------------------------------------------------------------
 (setq shell-file-name "/bin/zsh"
       vterm-max-scrollback 5000)
 (setq eshell-rc-script "~/.config/doom/eshell/profile"
@@ -176,17 +204,3 @@
       :desc "Eshell popup toggle"    "e t" #'+eshell/toggle
       :desc "Counsel eshell history" "e h" #'counsel-esh-history
       :desc "Vterm popup toggle"     "v t" #'+vterm/toggle)
-
-;;org-download
-;;This extension facilitates moving images from browser filesystems
-(require 'org-download)
-;; Drag-and-drop to `dired`
-(add-hook 'dired-mode-hook 'org-download-enable)
-;; Screen captured is saved on particular folder
-(after! org-download
-  (setq-default org-download-image-dir "/mnt/Backup/Org/notes_images"))
-;; custom keybinding for org-download
-(map! :leader
-      (:prefix ("i v" . "Org Download Mode")
-        :desc "Paste Image"    "a" #'org-download-clipboard
-        :desc "Screen Capture" "s" #'org-download-screenshot))
